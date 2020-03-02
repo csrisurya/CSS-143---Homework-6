d629c66f0ba211f6fc6f4d39fde10bfa38f11229
Srisurya Chandramouli <csrisurya@gmail.com>
2020-03-02 07:50:41 -0800

    Worked on index()

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
@@ -18,7 +17,25 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-        //homework
+        indexes = new HashMap<>();
+
+        for(int i = 0; i < texts.size(); i++){
+            String[] array = texts.get(i).split(" ");
+
+            for(int j = 0; j < array.length; j++){
+                List<List<Integer>> bigList = new ArrayList<>();
+                for(int k = 0; k < texts.size(); k++){
+                    bigList.add(new ArrayList<>());
+                }
+                if(!indexes.containsKey(array[j])){
+                    indexes.put(array[j], bigList);
+                }
+                indexes.get(array[j]).get(i).add(j);
+
+            }
+        }
+        System.out.println(indexes.toString());
+
     }
 
     // search(key) return all the document ids where the given key phrase appears.
@@ -28,4 +45,6 @@ public class MyMiniSearchEngine {
         // homework
         return new ArrayList<>(); // place holder
     }
+
+
 }
